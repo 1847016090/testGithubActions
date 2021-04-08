114823a75a27c51e058da18cc95f511d56eaabcc
1847016090 <1847016090@qq.com>
2021-04-09 00:38:53 +0800

    feat: stephen blog

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
 function App() {
-  console.log('1', 1)
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
         stephen blog
-      </header>
     </div>
   );
 }
