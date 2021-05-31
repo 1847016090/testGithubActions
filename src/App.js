cf1601e081c4a82349da564d2491cdf7d4897577
1847016090 <1847016090@qq.com>
2021-06-01 00:54:23 +0800

    fix: fix 简历

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ function App() {
       <div>
         <h3 class="header-title">工作经历</h3>
         <div className="_flex _between">
-          <div>{showInfo("成都xxx技术股份有限公司")} | 前端开发工程师</div>
+          <div>{showInfo("成都聚思力技术股份有限公司")} | 前端开发工程师</div>
           <div>{showInfo("2018年10月 - 至今")}</div>
         </div>
       </div>
@@ -56,37 +56,30 @@ function App() {
         <h3 className="header-title">项目经历</h3>
         <div>
           <div className="_bold">STELLR 平台</div>
-          <div>
-            STELLR一个是对微软，谷歌,
-            Office365等售卖云服务的B端平台。系统包含：用户管理，报表分析，设备管理，消息管理，埋点等。
-          </div>
+          <div>STELLR一个是对微软，谷歌， Office365等售卖云服务的B端平台。</div>
+          <div>系统包含：用户管理，报表分析，设备管理，消息管理，埋点等。</div>
           <div>
             项目采用 Dva(基于React，Redux，React-Router) 为脚手架，
-            在其基础上集成了 immutable, eslint，react-intl,
+            在其基础上集成了 immutable，eslint，react-intl，以及公司封装的
             solv-uicomponent(基于antd封装)，
             solv-models(管理业务逻辑),solv-common(提供基础方法)
           </div>
-          <div className="_bold">个人职责</div>
+          <div className="_bold _m_t_half _m_b_half">个人职责</div>
           <ul>
-            <li>负责用户管理，设备管理的页面开发以及迭代以及页面埋点</li>
+            <li>负责用户管理，设备管理的页面开发以及迭代，页面埋点</li>
             <li>讨论组件使用标准，完善组件库，以及根据标准编写组件示例</li>
             <li>开发Vendor静态页面</li>
             <li>快速编写业务deme演示给客户</li>
           </ul>
-          <div className="_bold">项目难点</div>
+          <div className="_bold _m_t_half _m_b_half">项目难点</div>
           <ul>
             <li>
-              使用虚拟列表解决长列表性能问题
+              使用Vue + Koa
+              完成定制化翻译工具，解决项目在上线前需要手动翻译上百条数据的问题并且获得同事好评。
             </li>
-            <li>
-              使用Vue + Koa 完成定制化翻译工具，解决项目在上线前需要手动翻译上百条数据的问题并且获得同事好评。
-            </li>
-            <li>
-              整理Solv-Ui 组件使用用例代码块并分享给同事,提高开发效率
-            </li>
-            <li>
-              手写简单Cli工具，快捷下载使用模版
-            </li>
+            <li>在滚动加载中使用虚拟列表解决长列表性能问题</li>
+            <li>整理Solv-Ui 组件使用用例代码块并分享给同事,提高开发效率</li>
+            <li>手写简单Cli工具，快捷下载使用模版</li>
           </ul>
         </div>
       </div>
@@ -101,9 +94,7 @@ function App() {
           </li>
           <li>熟悉Redux，React-Reudx，React-Router全家桶以及实现原理</li>
           <li>熟悉git版本控制工具、npm包管理器</li>
-          <li>
-            熟悉Webpack，Eslint，Prettier等工程化构建工具
-          </li>
+          <li>熟悉Webpack，Eslint，Prettier等工程化构建工具</li>
           <li>熟悉Dva, 了解 Tarojs 等等脚手架</li>
           <li>了解Express， Koa框架，能使用其实现简单的服务器</li>
           <li>熟练阅读和查阅计算机相关英文文档，并且有良好的编程习惯。</li>
@@ -117,7 +108,7 @@ function App() {
           <li>行走的标准，总是能在和设计的沟通中提出设计不规范的地方。</li>
           <li>具有良好的文化素质，脾气很好，能够和同事和谐相处</li>
           <li>工作积极，能够独立工作，又有团队精神</li>
-          <li>热爱运动，喜欢篮球，唱歌</li>
+          <li>热爱运动，喜欢篮球，唱歌，健身</li>
         </ul>
       </div>
     </div>
