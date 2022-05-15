24a8ab59a56dea2b9e1db923c3191fff626bb98a
1847016090 <1847016090@qq.com>
2022-05-15 13:31:23 +0800

    fix: 更新简历

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           <img src={avatartImg} alt="" />
         </div>
         <div className="_w_6 _flex _v _between">
-          <h2>{showInfo("叶康 | 在职 | 求职意向(前端开发工程师)")}</h2>
+          <h2>{showInfo("叶康 | 已提离职 | 求职意向(前端开发工程师)")}</h2>
           <div className="_flex _between">
             <div className="_w_2">
               <div>性　　别：男</div>
@@ -73,24 +73,28 @@ function App() {
       <div>
         <h3 className="header-title">项目经历</h3>
         <div>
-          <div className="_bold _m_b_1">2021年9月 - 至今：鱼泡网小程序重构</div>
-          <div>鱼泡网小程序是一个为全国工友提供找活的一个平台</div>
-          <div>项目技术：Taro + icestore + Typescript</div>
+          <div className="_bold _m_b_1">
+            2021年10月 - 至今：鱼泡网小程序重构
+          </div>
+          <div>
+            鱼泡网是一个建筑工程招工找活平台，4000万的注册工友，每天实时发布附近工地真实有效的工程招聘信息，方便建筑工人找老板、找项目、找工作。
+          </div>
+          <div>项目技术：Taro + Typescript + icestore</div>
           <div className="_bold _m_t_half _m_b_half">个人职责</div>
           <ul className="_m_b_2">
             <li>
-              日常迭代开发：企业认证接入，第三方E证通人脸识别接入，发布招工地址优化，找活名片优化等
+              担任生产线核心开发，积极协调同事工作，并负责实名，企业认证接入，第三方E证通人脸识别接入，发布招工地址优化，找活名片优化等迭代开发维护
             </li>
             <li>
-              推动并且集成Eslint，Prettier,
-              Husky等，统一代码编写规范，代码提交规范
-            </li>
-            <li>
-              优化页面性能，让小程序页面在低端机上面的显示速度提升一倍以上
+              优化招工，找活页面性能，降低进入页面的白屏时间，让小程序页面在低端机上面的显示速度提升一倍以上
             </li>
             <li>重构工种组件，城市组件，封装图片组件，相机组件等公用组件</li>
             <li>
-              封装History，useStates，ypStorage等工具方法极大的提升了团队的开发效率
+              推动框架从Taro2.0升级到3.0，并集成Eslint，Prettier，
+              Husky等，统一代码编写规范，代码提交规范
+            </li>
+            <li>
+              封装History路由方法，ypStorage缓存方法；useStates，usePreventRepeatedCalls自定义Hooks方法提升了页面的性能
             </li>
             <li>
               推动前后数据结构对接规范；集成mock数据
@@ -108,41 +112,33 @@ function App() {
             </li>
           </ul>
         </div>
+
         <div>
           <div className="_bold _m_b_1">
-            2018年10月 - 2021年9月：STELLR 云平台
+            2018年10月 - 2021年10月：STELLR 云平台
           </div>
           <div>
-            STELLR一个是为微软，谷歌， Office365等供应商售卖云服务的B端平台。
+            {/* STELLR一个是为微软，谷歌， Office365等供应商售卖云服务的B端平台。 */}
+            2019年02月-2022年03月 STELLR
+            是面向欧洲，美洲地区的B端云产品销售平台，合作方包括苹果，谷歌，微软等300+供应商，和20000+下游公司，是公司营收的三大马车之一。
           </div>
-          {/* <div>系统包含：用户管理，报表分析，设备管理，消息管理，埋点等。</div> */}
           <div>项目技术：React + Redux + Dva，在其基础上集成了react-intl</div>
           <div className="_bold _m_t_half _m_b_half">
             {"个人职责 & 解决问题"}
           </div>
           <ul>
             <li>
-              日常迭代开发：用户管理后台，设备管理后台，用户协作平台等页面开发以及迭代
+              担任生产线核心开发，负责用户管理后台，设备管理后台，用户协作平台等页面开发以及迭代
             </li>
             <li>讨论组件使用标准，完善组件库，以及根据标准编写组件示例</li>
             {/* <li>讨论组件使用标准，完善组件库，主导Storybook搭建展示用例并且据标准编写组件示例</li> */}
             <li>
               工作之余完成定制化翻译工具，解决项目在上线前需要手动翻译上百条数据的问题大大提高同事的工作效率。
             </li>
-            <li>滚动加载中研究并使用虚拟列表极大的提高了长列表滚动的性能</li>
+            <li>滚动加载研究并使用虚拟列表极大的提高了长列表滚动的性能</li>
             <li>手写Cli工具，提供快捷下载不同项目模版代码</li>
             <li>整理Solv-Ui 组件使用用例代码块并分享给同事，提高开发效率</li>
           </ul>
-          {/* <div className="_bold _m_t_half _m_b_half">项目难点</div>
-          <ul>
-            <li>
-              使用Vue + Koa
-              完成定制化翻译工具，解决项目在上线前需要手动翻译上百条数据的问题并且获得同事好评。
-            </li>
-            <li>滚动加载中使用虚拟列表解决长列表性能问题</li>
-            <li>整理Solv-Ui 组件使用用例代码块并分享给同事，提高开发效率</li>
-            <li>手写简单Cli工具，快捷下载使用模版</li>
-          </ul> */}
         </div>
       </div>
       {/* 自我评价 */}
